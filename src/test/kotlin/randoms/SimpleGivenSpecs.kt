77c783941f9320b50ffc7425e4203635c1465ec9
Dave Smith <dave.k.smith@gmail.com>
2018-06-10 20:29:22 -0500

    significant simplification

--- src/test/kotlin/randoms/SimpleGivenSpecs.kt
+++ src/test/kotlin/randoms/SimpleGivenSpecs.kt
@@ -1,6 +1,6 @@
 package randoms
 
-import org.amshove.kluent.`should be`
+import kotlin.test.assertEquals
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -8,47 +8,18 @@ import org.jetbrains.spek.api.dsl.on
 
 class SimpleGivenSpec : Spek({
 
-    given("the number x equal 6") {
-        val x = 6
+  given("the number x equal 6") {
+    val x = 6
 
-        given("the number y equal 7") {
-            val y = 7
+    on("multiplying x and 7") {
+      val actualAnswer = multiply(x, 7)
 
-            on("multiplying x and y") {
-                val actualAnswer = multiply(x, y)
-
-                it("should equal 42") {
-                    actualAnswer `should be` 42
-                }
-            }
-        }
+      it("should equal 42") {
+        assertEquals(actualAnswer, 42)
+      }
     }
-})
-
-class SimpleGivenSpec2 : Spek({
-
-    given("the number x equal 2") {
-        val x = 2
-
-        on("squaring x") {
-            val actualAnswer = squared(x)
-
-            it("should equal 4") {
-                actualAnswer `should be` 4
-            }
-        }
-        on("cubing x") {
-            val actualAnswer = cubed(x)
-
-            it("should equal 8") {
-                actualAnswer `should be` 8
-            }
-        }
-    }
+  }
 })
 
 fun multiply(a: Int, b: Int) = a * b
 
-fun squared(a: Int) = a * a
-
-fun cubed(a: Int) = a * a * a
